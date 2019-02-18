7ae8f128caad8a93dd83de0362ff520e85b6baa1
Ubuntu <ubuntu@ip-172-31-35-212.ap-southeast-2.compute.internal>
2019-02-18 10:23:27 +1000

    Changed extension to gsq_theme

---
+++ ckanext/gsq_theme/plugin.py
@@ -0,0 +1,33 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class Qld_Gov_ThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IPackageController, inherit=True)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'qld_gov_theme')
+
+    def before_search(self, data_dict):
+    	if 'seismic' in data_dict['fq'] and 'geochemistry' in data_dict['fq']:
+	    	fix = "tags:(seismic OR geochemistry)"
+	    	data_dict['fq'] = fix
+    	return data_dict
+
+    # The permit route for header pill navigation button
+    # Reference: https://stackoverflow.com/questions/17777191/how-to-add-a-menu-item-to-ckans-naivigation-menu
+    # def before_map(self, m):
+    # 	m.connect(
+    # 		'dataset',
+    # 		'/dataset',
+    # 		controller='ckanext.qld_gov_theme.controller:NewDatasetController',
+    # 		action='dataset'
+    # 	)
+    # 	# return toolkit.url_for('http://www.google.com')
+    # 	return m
