78107b4716d8416e221165ea86d62d6096e92205
Eric McCowan <45058635+ericmccowan@users.noreply.github.com>
2019-05-31 15:31:04 +1000

    Removed before_search()
    Adding tags in this way caused an Internal Server Error when using the package_show API call

--- ckanext/gsq_theme/plugin.py
+++ ckanext/gsq_theme/plugin.py
@@ -66,29 +66,29 @@ class Qld_Gov_ThemePlugin(plugins.SingletonPlugin):
             'get_type': get_dataset_type,
         }
 
-    def before_search(self, data_dict):
-        """Create a UNION of the search results containing the desired tags."""
-        args = data_dict['fq'].split(' ')
-        tags = []
-        for arg in args:
-            if 'tags:' in arg and 'seismic' in arg:
-                tags.append('seismic')
-            if 'tags:' in arg and 'geochemistry' in arg:
-                tags.append('geochemistry')
-            if 'tags:' in arg and 'dataset' in arg:
-                tags.append('dataset')
+# def before_search(self, data_dict):
+#         """Create a UNION of the search results containing the desired tags."""
+#         args = data_dict['fq'].split(' ')
+#         tags = []
+#         for arg in args:
+#             if 'tags:' in arg and 'seismic' in arg:
+#                 tags.append('seismic')
+#             if 'tags:' in arg and 'geochemistry' in arg:
+#                 tags.append('geochemistry')
+#             if 'tags:' in arg and 'dataset' in arg:
+#                 tags.append('dataset')
         
-        if tags:
-            fix = 'tags:('
-            for i, tag in enumerate(tags):
-                if i > 0:
-                    fix += ' OR '
-                fix += tag
+#         if tags:
+#             fix = 'tags:('
+#             for i, tag in enumerate(tags):
+#                 if i > 0:
+#                     fix += ' OR '
+#                 fix += tag
             
-            fix += ')'
-            data_dict['fq'] = fix
+#             fix += ')'
+#             data_dict['fq'] = fix
 
-    	return data_dict
+#     	return data_dict
 
     # The permit route for header pill navigation button
     # Reference: https://stackoverflow.com/questions/17777191/how-to-add-a-menu-item-to-ckans-naivigation-menu
